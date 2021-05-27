af0c46cc199223df483f130d6944e53fd1e1d22a
juberr <juberr1@live.ca>
2021-05-27 16:18:21 -0400

    get stability with waits, writes to csv

--- pharmacybooking.py
+++ pharmacybooking.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from urllib import parse
 import csv
 import re
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
 driver = webdriver.Chrome('./chromedriver')
+
 
 
 def parse_url_data(url, avail):
@@ -21,21 +22,38 @@ def parse_url_data(url, avail):
     province = address.split(',')[-1].split()[0]
     uuid = queries['owner'][0]
     availability = bool(avail)
-    row = [name, address, postal_code, province, uuid, availability]
+    row = [name, address, postal_code, province, uuid, availability, url]
 
     return row
 
 def click(driver, item):
-        driver.find_element_by_tag_name("select").send_keys(item)
-        driver.find_element_by_class_name('btn-cta').click()
-        driver.implicitly_wait(1)
 
-def scrape_pharm_booking(driver):
+    select_field = WebDriverWait(driver, 5).until(ec.visibility_of_element_located((By.TAG_NAME, 'select')))
+    select_field.send_keys(item)
+    button = WebDriverWait(driver, 5).until(ec.visibility_of_element_located((By.CLASS_NAME, 'btn-cta')))
+    button.click()
+
+def get_options(driver, time, data):
+
+    options = WebDriverWait(driver, time).until(ec.visibility_of_element_located((By.TAG_NAME, 'option')))
+    options = driver.find_elements_by_tag_name('option')
+
+    if data == 'value':
+        options = [i.get_attribute('value') for i in options if i.get_attribute('value') not in ['','Select']]
+        return options
+    
+    options = [i.text for i in options if i.text not in ['','Select']]
+
+    return options
+
+def scrape_pharm_booking(driver, file_writer):
 
     driver.get('https://www.pharmacybooking.com/')
-    driver.find_element_by_class_name('btn-cta').click()
-    provinces = driver.find_elements_by_tag_name('option')
-    provinces = [i.get_attribute('value') for i in provinces if i.get_attribute('value') != '']
+    button = WebDriverWait(driver, 5).until(ec.visibility_of_element_located((By.CLASS_NAME, 'btn-cta')))
+
+    button.click()
+
+    provinces = get_options(driver, 5, 'value')
 
     for province in provinces:
 
@@ -43,57 +61,65 @@ def scrape_pharm_booking(driver):
 
         try:
 
-            error = driver.find_element_by_class_name('error').text # not participating
+            WebDriverWait(driver, 5).until(ec.visibility_of_element_located((By.CLASS_NAME, 'error')))
+             # not participating
             print(f'{province} not participating, skipping')
-            driver.back()
+            backbutton = driver.find_element_by_class_name('btn')
+            backbutton.click()
             continue
 
-        except NoSuchElementException:
+        except TimeoutException:
 
-            options = driver.find_element_by_tag_name("select").find_elements_by_tag_name("option")
+            options = WebDriverWait(driver, 2).until(ec.visibility_of_element_located((By.TAG_NAME, 'select')))
+            options = driver.find_elements_by_tag_name('option')
             # looks like they currently have one field for all covid-19 vaccination appointments
             try:
                 covid_options = [option.text for option in options if 'covid 19' in option.text.lower()]
+                covid_options[0]
             except IndexError:
                 print(f'{province} has no covid-19 vaccine offerings, skipping')
+                backbutton = driver.find_element_by_class_name('btn')
+                backbutton.click()
                 continue
 
-            for covid_option in covid_options:
+            # for covid_option in covid_options:
 
-                click(driver, covid_option)
+            click(driver, covid_options[0])
 
-                cities = driver.find_element_by_tag_name("select").find_elements_by_tag_name("option")
-                cities = [city.text for city in cities if city.text != 'Select']
+            cities = get_options(driver, 5, 'value')
 
-                for city in cities:
+            for city in cities:
 
-                    click(driver, city)
+                click(driver, city)
 
-                    pharmacies = driver.find_element_by_tag_name("select").find_elements_by_tag_name("option")
-                    pharmacies = [pharm.text for pharm in pharmacies if pharm.text != 'Select']
+                pharmacies = get_options(driver, 5, 'text')
+                print(pharmacies)
 
-                    for pharmacy in pharmacies:
+                for pharmacy in pharmacies:
 
-                        click(driver, pharmacy)
+                    click(driver, pharmacy)
 
-                        iframe = driver.find_element_by_tag_name('iframe')
-                        driver.switch_to.frame(iframe)
-                        avail =  driver.find_elements_by_class_name('activeday')
-                        driver.switch_to.default_content()
-                        url = iframe.get_attribute('src')
-                        print(parse_url_data(url, avail))
-                        driver.back()
-
+                    iframe = WebDriverWait(driver, 5).until(ec.visibility_of_element_located((By.TAG_NAME, 'iframe')))
+                    url = iframe.get_attribute('src')
+                    driver.get(url)
+                    avail =  driver.find_elements_by_xpath("//span[@data-qa='activeCalendarDay']")
+                    data = parse_url_data(url, avail)
+                    file_writer.writerow(data)
+                    driver.back()
                     driver.back()
 
                 driver.back()
 
             driver.back()
 
+        driver.back()
+
             
 
                     
-            
-scrape_pharm_booking(driver)
+with open('scrape.csv', mode='w') as file:
+    file_writer = csv.writer(file, delimiter=',')
+    file_writer.writerow(['Name', 'Address', 'Postal Code', 'Province', 'UUID', 'Availability', 'URL'])
+    scrape_pharm_booking(driver, file_writer)
 
 driver.quit()
